ff2f6856c64093f06a2b6c36a72ff2ab51535529
Mahmoud-Elshony <101846669+Mahmoud-Elshony@users.noreply.github.com>
2024-07-22 22:03:59 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,7 @@ function handelClick(imgElem, imgIndex) {
       if (firstImg.src == secondImg.src) {
         score++;
         if(score==6){
-            restartBtn.hidden = true
+            restartBtn.hidden = false;
             winnnerAudio.play();
             return;
         }
